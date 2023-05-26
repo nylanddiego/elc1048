c7afdfa5aa8f34dab5f13dd39ecb93611162c0c3
nylanddiego <nyland.diego@acad.ufsm.br>
2023-05-25 22:46:26 -0300

    Exercício 1 - sistemas operacionais multitarefas

--- MULTITAREFAS/MULTITAREFAS_R21/src/main.c
+++ MULTITAREFAS/MULTITAREFAS_R21/src/main.c
@@ -17,8 +17,8 @@
  * \par Conteudo
  *
  * -# Inclui funcoes do sistema multitarefas (atraves de multitarefas.h)
- * -# Inicialização do processador e do sistema multitarefas
- * -# Criação de tarefas de demonstração
+ * -# Inicializaï¿½ï¿½o do processador e do sistema multitarefas
+ * -# Criaï¿½ï¿½o de tarefas de demonstraï¿½ï¿½o
  *
  */
 
@@ -34,6 +34,7 @@
  */
 void tarefa_1(void);
 void tarefa_2(void);
+void tarefa_nova(void);
 void tarefa_3(void);
 void tarefa_4(void);
 void tarefa_5(void);
@@ -46,6 +47,7 @@ void tarefa_8(void);
  */
 #define TAM_PILHA_1			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_2			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_nova		(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_3			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_4			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_5			(TAM_MINIMO_PILHA + 24)
@@ -59,6 +61,7 @@ void tarefa_8(void);
  */
 uint32_t PILHA_TAREFA_1[TAM_PILHA_1];
 uint32_t PILHA_TAREFA_2[TAM_PILHA_2];
+uint32_t PILHA_TAREFA_nova[TAM_PILHA_nova];
 uint32_t PILHA_TAREFA_3[TAM_PILHA_3];
 uint32_t PILHA_TAREFA_4[TAM_PILHA_4];
 uint32_t PILHA_TAREFA_5[TAM_PILHA_5];
@@ -80,6 +83,8 @@ int main(void)
 	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 1);
 	
 	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 2);
+	
+	CriaTarefa(tarefa_nova, "Tarefa nova", PILHA_TAREFA_nova, TAM_PILHA_nova, 3);
 	
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
@@ -103,6 +108,7 @@ void tarefa_1(void)
 	for(;;)
 	{
 		a++;
+		TarefaContinua(3);
 		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE); /* Liga LED. */
 		TarefaContinua(2);
 	
@@ -115,8 +121,20 @@ void tarefa_2(void)
 	for(;;)
 	{
 		b++;
-		TarefaSuspende(2);	
 		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE); 	/* Turn LED off. */
+		TarefaSuspende(2);	
+	}
+}
+
+void tarefa_nova(void)
+{
+	volatile uint16_t c = 0;
+	for(;;)
+	{
+		c++;	
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE); 	/* Turn LED on. */
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE); 	/* Turn LED off. */
+		TarefaSuspende(3);
 	}
 }
 
@@ -155,12 +173,12 @@ semaforo_t SemaforoTeste = {0,0}; /* declaracao e inicializacao de um semaforo *
 void tarefa_5(void)
 {
 
-	uint32_t a = 0;			/* inicializações para a tarefa */
+	uint32_t a = 0;			/* inicializaï¿½ï¿½es para a tarefa */
 	
 	for(;;)
 	{
 		
-		a++;				/* código exemplo da tarefa */
+		a++;				/* cï¿½digo exemplo da tarefa */
 
 		TarefaEspera(3); 	/* tarefa se coloca em espera por 3 marcas de tempo (ticks) */
 		
@@ -173,19 +191,19 @@ void tarefa_5(void)
 void tarefa_6(void)
 {
 	
-	uint32_t b = 0;	    /* inicializações para a tarefa */
+	uint32_t b = 0;	    /* inicializaï¿½ï¿½es para a tarefa */
 	
 	for(;;)
 	{
 		
-		b++; 			/* código exemplo da tarefa */
+		b++; 			/* cï¿½digo exemplo da tarefa */
 		
 		SemaforoAguarda(&SemaforoTeste); /* tarefa se coloca em espera por semaforo */
 
 	}
 }
 
-/* soluçao com buffer compartihado */
+/* soluï¿½ao com buffer compartihado */
 /* Tarefas de exemplo que usam funcoes de semaforo */
 
 #define TAM_BUFFER 10
@@ -197,7 +215,7 @@ semaforo_t SemaforoVazio = {TAM_BUFFER,0}; /* declaracao e inicializacao de um s
 void tarefa_7(void)
 {
 
-	uint8_t a = 1;			/* inicializações para a tarefa */
+	uint8_t a = 1;			/* inicializaï¿½ï¿½es para a tarefa */
 	uint8_t i = 0;
 	
 	for(;;)
@@ -240,7 +258,7 @@ void tarefa_8(void)
 		valor = buffer[f];
 		f = (f+1) % TAM_BUFFER;	
 		
-		(void)valor;	/* leitura da variável para evitar aviso (warning) do compilador */
+		(void)valor;	/* leitura da variï¿½vel para evitar aviso (warning) do compilador */
 		
 		SemaforoLibera(&SemaforoVazio);
 	}
